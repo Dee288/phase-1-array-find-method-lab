a53ce5b95eaefc2650d8a3db191e011b32827de0
dee288 <dianamajala10@gmail.com>
2022-06-21 08:20:16 +0300

    commit tree 96861848b44e9c997376097cfa6dea07805694cc

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const result = record.find(function(superbowlRecord) {
+    if ( record.result === "W"){
+        return !!result ? result.year : undefined
+    }
+
+}
+)
+
+
+
+
+
+function superbowlWin(record){
+
+    const result = record.find( record => record.result === "W" )
+    return !!result ? result.year : undefined
+   }
+   
