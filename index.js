80f975f94f89f2a8051d2e8d8a5d287d0988a617
dee288 <dianamajala10@gmail.com>
2022-06-21 08:24:11 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const result = record.find(function(superbowlRecord) {
+    if ( record.result === "W"){
+      return !!result ? result.year : undefined
+    }
+
+}
+)
+
+
+
+
+
+
